4693a3110f59d6ea54d2566dab2f7203c25b4163
mikemartinez1974 <mike.d.martinez@gmail.com>
2022-11-15 23:25:32 +0700

    A lot of changes.

--- scratch.js
+++ scratch.js
@@ -1,43 +1,10 @@
-const fs = require('fs');
-eval(fs.readFileSync('c:/users/michael/documents/sourcecode/openai/askopenai.js') + '');
-eval(fs.readFileSync('c:/users/michael/documents/sourcecode/data/datatools.js') + '');
-eval(fs.readFileSync('c:/users/michael/documents/sourcecode/utils/utils.js') + '');
+Worldwide AI Hackathon is a flagship event hosted by Wow AI in which talented teams of students 
+from top universities, developers and data scientists from global corporations, and AI enthusiasts
+meet to compete in challenges designed by top AI thought-leaders who are AI executives, 
+directors, Vice presidents, and senior managers from nearly 30 tech giants and large corporations, 
+including but not limited to Google, Oracle, Microsoft, IBM, Qualcomm, US Air Force, Intel, 
+Mastercard, Samsung, Oxford, Stanford, Deloitte, Accenture, Airbus, Volkswagen, Walmart, and many 
+others.
 
-async function begin() {
-    console.log(" * BEGIN * ");
-    let myArray = await execute("select school from scraperdata.schools");
-    //console.log(myArray);
-
-    schools = await execute("select school from scraperdata.schools");
-    let schoolGroups = makeGroups(schools,3);
-
-    //console.log(schoolGroups)
-
-    let table = "deans";
-
-    let results;
-    let prompt = ""
-    for(let group in schoolGroups){
-        let s = "";
-        
-        for(let school in schoolGroups[group]){
-
-            s += "\n" + schoolGroups[group][school].school
-        }
-        let prompt = '`Give only truthful answers.\nList the deans at these schools:' + s + '\nTo answer, create JSON objects like this: { "school":"Charles Sturt University", "person":"Scott Bowman", "department":"science" }`';
-        //console.log(prompt);
-
-        results += await askDavinciList(prompt,[0],table)
-    }
-    
-    console.log(results);
-    process.exit();
-}
-
-
-
-begin().then(() => {
-    process.exit();
-});
-
-
+Our mission is to empower Al talents by challenging them to solve the most challenging real world 
+problems and motivating them to turn their ideas into complete products and commercialize them.
